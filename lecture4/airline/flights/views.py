aea0a12aa9a4a6c85011c46603fc62f54bc98ea5
anhlengoc <anhlengoc@vccorp.vn>
2020-09-18 18:23:45 +0700

    lecture4 80 percen

--- lecture4/airline/flights/views.py
+++ lecture4/airline/flights/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from .models import Flight, Airport, Passenger
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all(),
+        "airports": Airport.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all()
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
