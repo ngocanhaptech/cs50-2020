2d63224e12628839d56a90c4f76ad1e5f44d8035
anhlengoc <anhlengoc@vccorp.vn>
2020-11-03 11:42:06 +0700

    Section 5 update post

--- FlaskApi/section5/code/item.py
+++ FlaskApi/section5/code/item.py
@@ -12,6 +12,13 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return { 'message': 'Item not found.' }, 404
+    
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -19,17 +26,28 @@ class Item(Resource):
         result = cursor.execute(query, (name,))
         row = result.fetchone()
         connection.close()
-        
+
         if row:
             return { 'item': { 'name': row[0], 'price': row[1] } }
-        return { 'message': 'Item not found.' }, 404
-    
+
+
     def post(self, name):        
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if Item.find_by_name(name):
             return { 'message': "An item with name '{}' already exists.".format(name) }, 400
+            
         data = Item.parser.parse_args()
+
         item = { 'name':name, 'price': data['price'] }
-        items.append(item)
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
     
     def delete(self, name):
